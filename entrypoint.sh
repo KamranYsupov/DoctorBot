569442f56478c940afecfaea02d15f1e26fe8cb1
Kamran <kamrankek48@gmail.com>
2024-11-21 11:10:13 +0300

    entrypoint.sh bug refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level warning
+gunicorn web.core.wsgi:application --bind 0.0.0.0:8000 --log-level warning
